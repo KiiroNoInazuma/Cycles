85ead63855410599d225d066bb8f1d355acc677c
John Rey <adioskid@mail.ru>
2022-10-20 23:38:26 +0300

    Homework#3

--- src/Numbers.java
+++ src/Numbers.java
@@ -1,5 +1,13 @@
 public class Numbers {
     public static void main(String[] args) {
-        
+        int num = 0;
+        while (num < 10) {
+            num++;
+            System.out.print(num + "\t");
+        }
+        System.out.println();
+        for (num = 10; num > 0; num--) {
+            System.out.print(num + "\t");
+        }
     }
 }
