d152736ff443ace7e4ec93f2f21c18cc6317e995
John Rey <adioskid@mail.ru>
2022-10-22 18:03:47 +0300

    Homework#3

--- src/Friday.java
+++ src/Friday.java
@@ -1,7 +1,7 @@
 public class Friday {
     public static void main(String[] args) {
         for (int friday = 5; friday <= 31; friday += 7)
-            System.out.println("Сегодня пятница, " + friday + "-е число. Необходимо подготовить отечет.");
+            System.out.println("Сегодня пятница, " + friday + "-е число. Необходимо подготовить отечет!");
     }
 
 }
